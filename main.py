6399835e5962f61890e9d03f9f5b2db2804eb8bb
levi <2537791055@qq.com>
2025-03-28 17:44:07 +0800

    修改逻辑

--- main.py
+++ main.py
@@ -13,9 +13,9 @@ import re  # 添加正则表达式模块
 CONFIG_FILE = 'config.json'
 
 # Tesseract 环境配置
-os.environ["LANGDATA_PATH"] = r"D:\my_project\DeltaForceKeyBot-main\tessdata-4.1.0"
-os.environ["TESSDATA_PREFIX"] = r"D:\my_project\DeltaForceKeyBot-main\tessdata-4.1.0"
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+os.environ["LANGDATA_PATH"] = r"D:\my_project\DeltaForceKeyBotPlus\tessdata-4.1.0"
+os.environ["TESSDATA_PREFIX"] = r"D:\my_project\DeltaForceKeyBotPlus\tessdata-4.1.0"
+pytesseract.pytesseract.tesseract_cmd = r'E:\Program Files\Tesseract-OCR\tesseract.exe'
 
 # 全局变量
 keys_config = None
@@ -41,8 +41,10 @@ def load_keys_config():
             content = re.sub(r'//.*|#.*', '', content)
             config = json.loads(content)  # 解析为 JSON
             keys_config = config.get('keys', [])
-            isLoop = config.get('isLoop',False)
-            isDebug = config.get('isDebug',False)
+            isLoop = config.get('isLoop')
+            isDebug = config.get('isDebug')
+            print(f"isDebug: {isDebug}")
+            print(f"isLoop: {isLoop}")
             return keys_config
     except FileNotFoundError:
         print(f"[错误] 配置文件 {CONFIG_FILE} 不存在")
@@ -64,20 +66,28 @@ def take_screenshot(region, threshold):
     return binary_image
 
 def getCardPrice():
-    """获取当前卡片价格"""
-    region_width = int(screen_width * 0.1)  # 区域宽度为屏幕宽度的 10%
-    region_height = int(screen_height * 0.05)  # 区域高度为屏幕高度的 10%
-    region_left = int(screen_width * 0.856)
-    region_top = int(screen_height * 0.80)
-    region = (region_left, region_top+20, region_width -40, region_height-20)
+    """获取当前卡片价格，仅识别阿拉伯数字"""
+    region_width = int(screen_width * 0.08)  # 区域宽度为屏幕宽度的 8%
+    region_height = int(screen_height * 0.037)  # 区域高度为屏幕高度的 4%
+    region_left = int(screen_width * 0.851)
+    region_top = int(screen_height * 0.805)
+
+    region = (region_left, region_top, region_width, region_height)
 
     image = take_screenshot(region=region, threshold=55)
     image.save("./image.png")
-    # , config="--psm 13 -c tessedit_char_whitelist=0123456789,"
-    text = pytesseract.image_to_string(image, lang='eng')
+    text = pytesseract.image_to_string(image, lang='eng', config="--psm 13 -c tessedit_char_whitelist=0123456789")
+    print(f"原始文本: {text}")
 
+    # 如果第一个字符是 '0'，则移除它
+    text = text.replace(",", "") # 去除逗号
+    text = text.replace(" ", "") # 去除空格
+
+    # 因为会截取到价格左边的哈夫币logo，会被识别成 0，所以需要去掉
+    if text.startswith('0'):
+        text = text[1:]
     try:
-        price = int(text.replace(",", "").strip())
+        price = int(text)
         print(f"提取的价格文本: {price}")
         return price
     except ValueError:
@@ -90,14 +100,16 @@ def getCardName():
   region_width = int(screen_width * 0.1)  # 区域宽度为屏幕宽度的 10%
   region_height = int(screen_height * 0.05)  # 区域高度为屏幕高度的 10%
   region_left = int(screen_width * 0.768)
-  region_top = int(screen_height * 0.145)
-  region = (region_left, region_top - 25, region_width, region_height)
+  region_top = int(screen_height * 0.143)
+  region = (region_left, region_top, region_width, region_height)
 
   screenshot = take_screenshot(region=region, threshold=100)
   screenshot.save("./s.png")
   # 使用更适合中文识别的配置
   text = pytesseract.image_to_string(
-    screenshot, lang='chi_sim', config="--psm 7 -c tessedit_char_whitelist=0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz一二三四五六七八九十"
+    screenshot,
+    lang='chi_sim',  # 指定简体中文语言
+    config="--psm 7"  # 配置页面分割模式为 7（单行文本）
   )
   text = text.replace(" ", "").strip()  # 清理空格和换行符
   return text
@@ -157,7 +169,8 @@ def price_check_flow(card_info):
     if premium < 0 or current_price < max_price:
         pyautogui.moveTo(screen_width * 0.825, screen_height * 0.852)
         if not isDebug:
-            pyautogui.click()  # 点击购买
+          print("正在购买...")
+          # pyautogui.click()  # 点击购买
 
         print(f"[+]已自动购买{card_name},理想价格为：{ideal_price},购买价格为：{current_price},溢价：{premium:.2f}%")
         log_purchase(card_name, ideal_price, current_price, premium)  # 调用日志记录函数
@@ -216,6 +229,8 @@ def main():
                     break
                 print(f"正在检查门卡: {card_info['name']}")
                 if price_check_flow(card_info):
+                  print(f"isDebug {isDebug}")
+                  print(f"isLoop {isLoop}")
                   if not isLoop:
                     cards_to_buy.remove(card_info)
                     print(f"剩余购买队列：{cards_to_buy}")
