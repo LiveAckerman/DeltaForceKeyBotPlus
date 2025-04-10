9e833e6a02a98d0265a9a8fc2da1690970566b79
levi <2537791055@qq.com>
2025-04-10 16:38:54 +0800

    修改bin/下面的快捷执行脚本乱码的问题

--- bin/run.cmd
+++ bin/run.cmd
@@ -1,12 +1,12 @@
-@echo off
-chcp 65001
+﻿@echo off
+REM 设置控制台编码为 UTF-8
+chcp 65001 >nul
 
 REM 检查是否具有管理员权限
 net session >nul 2>&1
 if %errorlevel%==0 (
     REM 如果有管理员权限，则切换到脚本目录并运行 Python 脚本
-    cd /d "%~dp0"
-    cd ..
+    cd /d "%~dp0\.."
     python ./main.py
 ) else (
     echo 当前未以管理员权限运行
--- bin/run_install.cmd
+++ bin/run_install.cmd
@@ -1,4 +1,5 @@
 @echo off
+REM 设置控制台编码为 UTF-8
 chcp 65001 >nul
 
 cd /d "%~dp0\.."
